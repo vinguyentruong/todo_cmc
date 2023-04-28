87a37648f25d861e81d45b4fdb9241d0fd4b7c15
tan vo <dangtan936@gmail.com>
2023-04-28 18:05:14 +0700

    add server grpc

---
+++ server/bin/server.dart
@@ -0,0 +1,8 @@
+import 'package:grpc/grpc.dart';
+import 'package:server/src/service/todo_service.dart';
+
+void main(List<String> arguments) async {
+  final server = Server([TodoService()]);
+  await server.serve(port: 50051);
+  print('server opened on port: ${server.port}');
+}
---
+++ server/lib/src/data/db.dart
@@ -0,0 +1,12 @@
+// https://jsonplaceholder.typicode.com/todos
+final List<Map<dynamic, dynamic>> todos = [
+  {
+    'id': '1',
+    'title': 'title ne',
+    'description': 'description',
+    'image': '',
+    'status': 'status',
+    'createdAt': 'createdAt',
+    'expiredAt': 'expiredAt'
+  },
+];
